1879e81b720e7bf507e0418a6e74c08f7b03f58f
legendx22 <74249238+legendx22@users.noreply.github.com>
2020-12-04 19:41:35 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,14 +7,14 @@ from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/93f85e88789464793bc5c.jpg"
 pm_caption = "⚠️ LEGEND BOT  is On 🔥 FIRE �⚠️ \n\n"
-pm_caption += "🔸**SYSTEM STATU**\n"
+pm_caption += "🔸**SYSTEM STATUS**\n"
 pm_caption += "🔹TELETHON VERSION : **6.0.9**\n ⭕️ Python: **3.7.4**\n"
 pm_caption += "🔸DATABASE STATUS  : **Functional**\n"
 pm_caption += "🔹**Current Branch** : `Master`\n"
 pm_caption += "🔸**LEGEND OS** :   1.14`\n"
 pm_caption += f"🔹**My Boss** : {DEFAULTUSER} \n"
 pm_caption += "🔸**Made By 😎** : [LEGEND](https://t.me/LEGENDX22)\n\n"
-pm_caption += "🔻Deploy LEGEND BIT : [ℝ𝕖𝕡𝕠](https://github.com/legendx22/LEGEND-BOT)\n"
+pm_caption += "🔻Deploy LEGEND BOT : [ℝ𝕖𝕡𝕠](https://github.com/legendx22/LEGEND-BOT)\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
