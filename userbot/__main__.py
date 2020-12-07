57c4cc572cab7c42224900f5e3fbf2eacb099024
legendx22 <74249238+legendx22@users.noreply.github.com>
2020-12-07 12:26:30 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Aree On Ho Gaya Bhenchod!! Abb jake .alive likh ke dekho!! Abb Jaao aur sabki maroo.... Join @HellBot_Official for any help..")
+print("Aree On Ho Gaya Bhenchod!! Abb jake .alive likh ke dekho!! Abb Jaao aur sabki maroo....  @LEGENDX22 for any help..")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
