09bcfd63133ba10bc2794dbec2c3dfc8431ea4fb
LEGEND X <74249238+legendx22@users.noreply.github.com>
2020-12-08 18:26:08 +0530

    H

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -10,20 +10,30 @@ from telethon.tl.types import ChannelParticipantsAdmins
 
 # uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/93f85e88789464793bc5c.jpg"
-pm_caption = "➥ **💥🅻🅴🅶🅴🅽🅳 𝔹𝕆𝕋💥 IS:** `ONLINE`\n\n"
-pm_caption += "➥ **👺🐚  т𝓔ˡ𝕖ţ𝐡𝕆Ⓝ 𝐕𝓔ŘsⓘØᑎ  👊♢:** `1.18.0` \n"
-pm_caption += "➥ **🄿🅈🅃🄷🄾🄽 ♡☞:** `3.7.4` \n"
-pm_caption += "➥ **👊  𝔡𝔞t𝓪в𝓪ⓢέ  𝕊𝕥𝕒𝕥𝕦𝕤  ♗:**  `Functional`\n"
-pm_caption += f"➥ **//💥  ✎  𝓂𝕪 βỖⓈş  ☞  💀//** \n {DEFAULTUSER} \n"
 
+""" =======================CONSTANTS====================== """
+PM_IMG = "https://telegra.ph/file/09924cb2a1bd1a9c733b2.mp4" 
+""" =======================CONSTANTS====================== """
+pm_caption = "➣    **🔥 LEGEND 𝐁𝐎𝐓 🔥 𝐈𝐒** `𝐎𝐍𝐋𝐈𝐍𝐄`\n\n"
+
+pm_caption +=  f"➥       **//__↼🄼🄰🅂🅃🄴🅁⇀__//**      \n 『{DEFAULTUSER}』 \n"
+pm_caption += " \n\n"
+pm_caption += "✘ ΔβØỮŦ Μ¥ Ş¥ŞŦ€Μ ✘\n\n"
+pm_caption += "➾ **𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍**  ➣ 𝟏.𝟏𝟕.𝟓\n"
+pm_caption += "➾ **𝐒𝐔𝐏𝐏𝐎𝐑𝐓 ** ➣ [𝐉𝐎𝐈𝐍](https://t.me/teamishere)\n"
+pm_caption += "➾ **𝐒𝐎𝐂𝐈𝐀𝐋  **  ➣ [𝐉𝐎𝐈𝐍](https://t.me/hackerget0)\n"
+pm_caption += "➾ **𝐂𝐑𝐄𝐀𝐓𝐎𝐑** ➣ [⚡ LEGEND⚡](t.me/legendx22)\n\n" 
+pm_caption += " \n\n"
+pm_caption += "[✨ Đ€ƤŁØ¥ ¥ØỮŘ ŞΔVΔǤ€ βØŦ ✨](https://github.com/sameerpanthi/SAVAGE) \n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
     chat = await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
+    
     await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
     await alive.delete()
+
 
     
 @borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
