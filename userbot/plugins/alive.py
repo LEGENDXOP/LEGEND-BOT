34b837987c8657374d806d6f84aa6aa3a2864710
LEGEND X <74249238+legendx22@users.noreply.github.com>
2020-12-08 15:00:31 +0530

    Update
    Upd

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,31 +1,45 @@
-#credits @LEGENDX22
 import asyncio
 from telethon import events
-from uniborg.util import admin_cmd
-from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot import ALIVE_NAME, hellversion
 from telethon.tl.types import ChannelParticipantsAdmins
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+
+ludosudo = Config.SUDO_USERS
+
+if ludosudo:
+    sudou = "True"
+else:
+    sudou = "False"
+
+kraken = bot.uid
+
 PM_IMG = "https://telegra.ph/file/93f85e88789464793bc5c.jpg"
-pm_caption = "⚠️ LEGEND BOT  is On 🔥 FIRE �⚠️ \n\n"
-pm_caption += "🔸**SYSTEM STATUS**\n"
-pm_caption += "🔹TELETHON VERSION : **6.0.9**\n ⭕️ Python: **3.7.4**\n"
-pm_caption += "🔸DATABASE STATUS  : **Functional**\n"
-pm_caption += "🔹**Current Branch** : `Master`\n"
-pm_caption += "🔸**LEGEND OS** :   1.14`\n"
-pm_caption += f"🔹**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "🔸**Made By 😎** : [LEGEND](https://t.me/LEGENDX22)\n\n"
-pm_caption += "🔻Deploy LEGEND BOT : [ℝ𝕖𝕡𝕠](https://github.com/legendx22/LEGEND-BOT)\n"
+pm_caption = "__**🔥🔥LEGEND BOT IS ON FIRE🔥🔥**__\n\n"
 
-@borg.on(admin_cmd(pattern=r"alive"))
-async def friday(alive):
+pm_caption += f"               _MY MASTER__\n**『[{DEFAULTUSER}](tg://user?id={kraken})』**\n\n"
+
+pm_caption += "🛡️TELETHON🛡️ : `1.15.0` \n"
+
+pm_caption += f"😏LEGEND BOT😏       : __**{1.2.2**__\n"
+
+pm_caption += f"😤Sudo😤            : `{sudou}`\n"
+
+pm_caption += "⚠️CHANNEL⚠️   : [ᴊᴏɪɴ](https://t.me/hackerget0)\n"
+
+pm_caption += "😱OWNER😱    : [Nub Here](https://t.me/LEGENDX22)\n\n"
+
+pm_caption += "    [✨REPO✨](https://github.com/legendx22/LEGEND-BOT) 🤐  [LICENSE](https://github.com/legendx22/LEGEND-BOG/blob/master/LICENSE)"
+
+pm_caption += " 😡Haters apni GA** maraao😡\n\n"
+
+@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
+@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+async def amireallyalive(alive):
+
+
     chat = await alive.get_chat()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
     await alive.delete()
-
-    
-@borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
-async def friday(alive):
-    chat = await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
     await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await alive.delete() 
