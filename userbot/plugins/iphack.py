1419212d3af896814d9c4d3a69c80778867b0ba2
LEGEND X <74249238+legendx22@users.noreply.github.com>
2020-12-13 13:33:54 +0530

    Add files via upload

---
+++ userbot/plugins/iphack.py
@@ -0,0 +1,58 @@
+#    Copyright (C) @chsaiujwal 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import json
+import urllib.request
+
+from uniborg.util import admin_cmd
+
+from userbot import CMD_HELP
+
+
+@borg.on(admin_cmd(pattern="ip (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+
+    adress = input_str
+
+    token = "19e7f2b6fe27deb566140aae134dec6b"
+
+    api = "http://api.ipstack.com/" + adress + "?access_key=" + token + "&format=1"
+
+    result = urllib.request.urlopen(api).read()
+    result = result.decode()
+
+    result = json.loads(result)
+    a = result["type"]
+    b = result["country_code"]
+    c = result["region_name"]
+    d = result["city"]
+    e = result["zip"]
+    f = result["latitude"]
+    g = result["longitude"]
+    await event.edit(
+        f"<b><u>INFORMATION GATHERED SUCCESSFULLY</b></u>\n\n<b>Ip type :-</b><code>{a}</code>\n<b>Country code:- </b> <code>{b}</code>\n<b>State name :-</b><code>{c}</code>\n<b>City name :- </b><code>{d}</code>\n<b>zip :-</b><code>{e}</code>\n<b>Latitude:- </b> <code>{f}</code>\n<b>Longitude :- </b><code>{g}</code>\n",
+        parse_mode="HTML",
+    )
+
+
+CMD_HELP.update(
+    {
+        "iplookup": "**IP LOOKUP**\
+\n\n**Syntax : **`.iplookup <ip address>`\
+\n**Usage :** Gives details about the ip address."
+    }
+)
