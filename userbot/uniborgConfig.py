192f0f9deec931bf5d1595ac86569914dbe6c6ab
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-24 21:47:38 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -136,7 +136,15 @@ if ENV:
             "UPSTREAM_REPO", "https://github.com/legendx22/LEGEND-BOT"
         )
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
-        
+        #ASSISTANT
+        ASSISTANT_LOG = int(os.environ.get("ASSISTANT_LOG", False))
+        ENABLE_ASSISTANTBOT = os.environ.get("ENABLE_ASSISTANTBOT", "ENABLE")
+        ASSISTANT_START_PIC = os.environ.get(
+        "ASSISTANT_START_PIC",
+        "https://www.logolynx.com/images/logolynx/72/7257d2ac93b97c8a2c7308266052df13.png",
+        )
+        BOT_HANDLER = os.environ.get("BOT_HANDLER", "^/")
+
 else:
     class Config(object):
         DB_URI = None
