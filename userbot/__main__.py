8362fdc532b3f16c52c281bc425f5c42b61b074f
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-02 13:43:02 +0530

    New

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Aree On Ho Gaya Bhenchod!! Abb jake .alive likh ke dekho!! Abb Jaao aur sabki maroo.... Join @HellBot_Official for any help..")
+print("LEGEND BOT is on fire 🔥🔥🔥 all files installed on your bot.... Join @teamishere for any help..")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
