7339e97b53e139d443a485aaa08f1c82d9a2bd95
lucifeermorningstar <76868561+lucifeermorningstar@users.noreply.github.com>
2021-01-05 08:33:33 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("LEGEND BOT is on fire 🔥🔥🔥 all files installed on your bot.... Join @teamishere for any help..")
+print("DEVIL USERBOT is on fire 🔥🔥🔥 all files installed on your bot.... Join @DEVILUSERBOT for any help..")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
