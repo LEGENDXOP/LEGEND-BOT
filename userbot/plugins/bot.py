b3d671869b8046ac75a64810742e1e2004671ba9
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-07 20:33:01 +0530

    Crash fix hua shayad

--- userbot/plugins/bot.py
+++ userbot/plugins/bot.py
@@ -1,17 +1,18 @@
 #make by legendx22 
+#credits @programmingerror
 #kang with keep credits
 
 from telethon import events, Button, custom
 @tgbot.on(events.InlineQuery)
 async def inline_handler(event):
-chut = event.builder
-butts = [[Button.url("REPO :gift_heart:", "https://github.com/legendx22/LEGEND-BOT"), Button.url('STRING SESSION :zap:', 'http://repl.it/@legendx22/LEGEND-BOT#main.py')]]
-query = event.text
-me = await borg.get_me()
-if query.startswith("repo") and event.query.user_id == me.id:
-    legend = chut.article(
-                       title="REPO OF LEGEND BOT",
-                       text="REPO OF LEGEND BOT + STRING SESSION",
-                       buttons=butts,
-                   )
-    await event.answer([legend])
+ chut = event.builder
+ butts = [[Button.url("REPO 💝", "https://github.com/legendx22/LEGEND-BOT"), Button.url('STRING SESSION ⚡ ', 'http://repl.it/@legendx22/LEGEND-BOT#main.py')]]
+ query = event.text
+ me = await borg.get_me()
+ if query.startswith("repo") and event.query.user_id == me.id:
+     legend = chut.article(
+                        title="REPO OF LEGEND BOT",
+                        text="REPO OF LEGEND BOT + STRING SESSION",
+                        buttons=butts,
+                    )
+     await event.answer([legend])
