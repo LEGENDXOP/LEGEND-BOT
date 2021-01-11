bcf957add6d74d2a49980ec9a151ddbd2247bc94
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-11 20:23:01 +0530

    Advanced loda by LEGEND X

--- userbot/plugins/loda.py
+++ userbot/plugins/loda.py
@@ -1,10 +1,39 @@
 import asyncio
+import time
 from datetime import datetime
 
-from .. import ALIVE_NAME, CMD_HELP
+from .. import ALIVE_NAME, StartTime, CMD_HELP
 from ..utils import admin_cmd, sudo_cmd, edit_or_reply
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND USER"
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+    
+uptime = get_readable_time((time.time() - StartTime))
 
 
 @borg.on(admin_cmd(pattern="loda$"))
@@ -18,7 +47,7 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(
-        f"__**✦҈͜͡➳ Lessun!__**\n★ immunity {ms}\n★ __**Mere**__ **Master ke pass abhi bht immunity h kisi ko bhi chod skte h check krna h kya ** [{DEFAULTUSER}](tg://user?id={ghanta})"
+        f"__**✦҈͜͡➳ Lessun!__**\n★ immunity {ms}\n★ __**Mere**__ **Master ka loda {uptime}  se khada h aur mere {DEFAULTUSER} ke pass abhi bht immunity h kisi ko bhi chod skte h check krna h kya abhi bhi khada h {uptime} hogaye  ** [{DEFAULTUSER}](tg://user?id={ghanta})"
     )
 
 
