d48444fc265c3263c6dcc0690e37ff389476bc03
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-14 14:12:06 +0530

    Sed

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -27,15 +27,13 @@ if ALIVE_PHOTTO is None:
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
 
-uptime = await legend.get_readable_time((time.time() - StartTime))
-
 ALIVE_MESSAGE= " ⚡ LEGEND BOT ⚡  IS ON 🔥 FIRE ⚜️ \n\n"
 ALIVE_MESSAGE += "🔱 SYSTEM STATUS🔱\n"
 ALIVE_MESSAGE += "🔱TELETHON VERSION🔱 : 6.0.9\n⭕ Python: 3.7.4\n"
 ALIVE_MESSAGE += "🔱DATABASE STATUS🔱 : Functional\n"
 ALIVE_MESSAGE += "🔱Current Branch🔱 : Master\n"
-ALIVE_MESSAGE += "🔱LEGEND TIME🔱 :   `{uptime}`\n"
-#op Bolte uptime add krna h 
+ALIVE_MESSAGE += "🔱LEGEND OS🔱 :   `LATEST`\n"
+#op Bolte uptime add krna h
 ALIVE_MESSAGE += f"⚜️ MY BOSS ⚜️: {DEFAULTUSER} \n"
 ALIVE_MESSAGE += "⚜️MADE BY⚜️ 😎 : [LEGEND X](https://t.me/legendx22)\n\n"
 ALIVE_MESSAGE += ":⚜️Deploy⚜️ **LEGEND BOT** : [ᖇᏋᎵᏫ](https://github.com/legendx22/LEGEND-BOT)\n"           
