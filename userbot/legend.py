8821e77418978f60557d436e80114ba37c732ca9
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-16 15:08:24 +0530

    Itna kaafi baaki baadme krte h

--- userbot/legend.py
+++ userbot/legend.py
@@ -8,3 +8,9 @@ LEGENDX22 = "[LEGEND X](https://t.me/legendx22)"
 OP = "[LEGEND BOT](https://github.com/legendx22/LEGEND-BOT)"
 OKAY = "[SUPPORT GROUP](https://t.me/LEGEND_USERBOT_SUPPORT)"
 #itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "LEGEND BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22
+BOT = " HELLO MASTER MY NAME IS LEGEND BOT I AM A BEST USERBOT 💝"
+EMOJI = "🔥"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
