23b9eb3f6da3ea617a81bd2a0106fc1f70fae747
LEGEND X <74249238+legendx22@users.noreply.github.com>
2021-01-16 13:15:50 +0530

    Work krjayio benchod

---
+++ userbot/legend.py
@@ -0,0 +1,10 @@
+#kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
+#make by legendx22 for chutiapa 🤔😑 you dont kang this 
+#okay to ab isme cheeze bharte h 
+#if you read only this then okay else
+#chala jaa bhosdike 
+
+LEGENDX22 = [LEGEND X](https://t.me/legendx22)
+OP = [LEGEND BOT](https://github.com/legendx22/LEGEND-BOT)
+OKAY = [SUPPORT GROUP](https://t.me/LEGEND_USERBOT_SUPPORT)
+#itna test h aur aage krte h
