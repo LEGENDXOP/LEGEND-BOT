c19c94222007951f3715a68532087b2fdb571746
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-01-17 21:13:00 +0530

    Duniya ka sabse bada chutia me hu

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -17,7 +17,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/legendx22/LEGEND-BOT"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/LEGENDXOP/LEGEND-BOT"
 BOT_IS_UP_TO_DATE = "❂LegendBOT is up-to-date!❂"
 NEW_BOT_UP_DATE_FOUND = (
     "**LEGEND BOT  Update Found For** {branch_name}\n"
