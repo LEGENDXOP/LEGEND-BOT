c12bd98a914bdfc3daf799b8ed5d4d6060e99682
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-01-17 21:15:08 +0530

    Sabse bada chutia me hu

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -142,7 +142,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/legendx22/LEGEND-BOT.git")
+    "https://github.com/LEGENDXOP/LEGEND-BOT.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
