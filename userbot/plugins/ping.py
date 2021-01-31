a73cea6422b8f68bd5ddc7340f6a82e14c77a4f1
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-01-31 16:40:05 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from .. import ALIVE_NAME, CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
 
 
 @borg.on(admin_cmd(pattern=f"hbping$", outgoing=True))
