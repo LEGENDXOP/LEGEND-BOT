5ec0c4b60efb334baf59a7846304891118fa7616
LEGENDXOP <LEGENDXOP@users.noreply.github.com>
2021-02-05 05:29:16 +0000

    Replaced new name

--- userbot/plugins/removebg.py
+++ userbot/plugins/removebg.py
@@ -61,7 +61,7 @@ async def _(event):
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "HELLBOT_RM_BG.png"
+            remove_bg_image.name = "LEGENDBOT_RM_BG.png"
             await borg.send_file(
                 event.chat_id,
                 remove_bg_image,
