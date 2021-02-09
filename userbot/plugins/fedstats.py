73889977470d043aa6a5c4c4fb520703e85866eb
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-02-09 19:33:27 +0530

    Update fedstats.py

--- userbot/plugins/fedstats.py
+++ userbot/plugins/fedstats.py
@@ -3,7 +3,7 @@ import asyncio
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot.legend import NAME
 from userbot import CMD_HELP, bot
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd
 
 bot = "@MissRose_bot"
 LEGENDX = NAME
