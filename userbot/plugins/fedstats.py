2e63e81154e468b56ee4ab553a4b49008be95dd5
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-02-09 19:24:04 +0530

    Update fedstats.py

--- userbot/plugins/fedstats.py
+++ userbot/plugins/fedstats.py
@@ -1,60 +1,75 @@
-# Plugin to show the fed you are banned in.
-# For TeleBot
-# Kangers keep credits
-# By @Akash_AM1 and @xditya
-
-import datetime
 import asyncio
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from telethon.tl.functions.messages import ImportChatInviteRequest
+# made by telebot
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot.legend import NAME
+from userbot import CMD_HELP, bot
 from userbot.utils import admin_cmd
-import time
-from userbot import ALIVE_NAME
-
-naam = str(ALIVE_NAME)
 
 bot = "@MissRose_bot"
+LEGENDX = NAME
 
-@borg.on(admin_cmd("fstat ?(.*)"))
+@telebot.on(admin_cmd(pattern="fstat ?(.*)"))
+@telebot.on(sudo_cmd(pattern="fstat ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
-        return    
-    sysarg = event.pattern_match.group(1)
+        return
+    ok = await event.edit(f"**CHECKING BY {LEGENDX}...")
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        sysarg = str(previous_message.sender_id)
+        user = f"[user](tg://user?id={sysarg})"
+    else:
+        sysarg = event.pattern_match.group(1)
+        user = sysarg
     if sysarg == "":
-      async with borg.conversation(bot) as conv:
-          try:
-              await conv.send_message("/start")
-              response = await conv.get_response()
-              await conv.send_message("/fedstat")
-              audio = await conv.get_response()
-              final = ("If you would like to know more about the fedban reason in a specific federation, use /fbanstat <FedID> in RoseBot." , "")
-              await borg.send_message(event.chat_id, audio.text)
-              await event.delete()
-          except YouBlockedUserError:
-              await event.edit("**Error:** `unblock` @MissRose_bot `and retry!")
-    elif "@" in sysarg:
-      async with borg.conversation(bot) as conv:
-          try:
-              await conv.send_message("/start")
-              response = await conv.get_response()
-              await conv.send_message("/fedstat " + sysarg)
-              audio = await conv.get_response()
-              final = ("If you would like to know more about the fedban reason in a specific federation, use /fbanstat <FedID> in RoseBot." , "")
-              await borg.send_message(event.chat_id, audio.text)
-              await event.delete()
-          except YouBlockedUserError:
-              await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
-    elif "" in sysarg:
-      async with borg.conversation(bot) as conv:
-          try:
-              await conv.send_message("/start")
-              response = await conv.get_response()
-              await conv.send_message("/fedstat " + sysarg)
-              audio = await conv.get_response()
-              final = ("If you would like to know more about the fedban reason in a specific federation, use /fbanstat <FedID> in RoseBot." , "")
-              await borg.send_message(event.chat_id, audio.text)
-              await event.delete()
-          except YouBlockedUserError:
-              await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
+        await ok.edit(
+            "`Give me someones id, or reply to somones message to check his/her fedstat.`"
+        )
+        return
+    else:
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/fedstat " + sysarg)
+                audio = await conv.get_response()
+                if "Looks like" in audio.text:
+                    await audio.click(0)
+                    await asyncio.sleep(2)
+                    audio = await conv.get_response()
+                    await telebot.send_file(
+                        event.chat_id,
+                        audio,
+                        caption=f"List of feds {user} has been banned in.\n\nFSTATS COLLECTED BY{LEGENDX}\n\nCollected by LEGEND BOT.",
+                    )
+                else:
+                    await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await ok.edit("**Error**\n `Unblock` @MissRose_Bot `and try again!")
+
+
+@telebot.on(admin_cmd(pattern="fedinfo ?(.*)"))
+@telebot.on(sudo_cmd(pattern="fedinfo ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    ok = await event.edit("`Extracting information...`")
+    sysarg = event.pattern_match.group(1)
+    async with borg.conversation(bot) as conv:
+        try:
+            await conv.send_message("/start")
+            await conv.get_response()
+            await conv.send_message("/fedinfo " + sysarg)
+            audio = await conv.get_response()
+            await ok.edit(audio.text + "\n\nFedInfo Excracted by TeleBot")
+        except YouBlockedUserError:
+            await ok.edit("**Error**\n `Unblock` @MissRose_Bot `and try again!")
+
+
+CMD_HELP.update(
+    {
+        "fedstuff": ".fstat <username/userid/reply to user>\nUse - To check the persons fedban stat in @MissRose_Bot.\
+        \n\n.fedinfo <fedid>\nUse - To see info about the fed."
+    }
+)
