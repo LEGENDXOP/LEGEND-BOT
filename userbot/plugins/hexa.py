b8e51c2763c3cf874fe2042479c2a9778f2ba8d2
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-02-13 16:15:24 +0530

    Update hexa.py

--- userbot/plugins/hexa.py
+++ userbot/plugins/hexa.py
@@ -1,6 +1,5 @@
-
-# credits LEGENDX22
-
+# MAKE BY LEGENDX22
+# credits PROBOY22 AND TEAMLEGEND
 from telethon import events
 import asyncio
 #from userbot.utils import admin_cmd
@@ -8,43 +7,9 @@ from userbot.events import register
 from userbot import bot, CMD_HELP
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 import os
-try:
- import subprocess
-except:
- os.system("pip install instantmusic")
- 
 
 
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
-    
-
-
-@register(outgoing=True, pattern="^.spd(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    link = event.pattern_match.group(1)
-    chat = "@hexamonbot"
-    await event.edit("```Hunting PokemOne```")
-    async with bot.conversation(chat) as conv:
-          await asyncio.sleep(2)
-          await event.edit("`catching It Wait Kro`")
-          try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
-              await bot.send_message(chat, link)
-              respond = await response
-          except YouBlockedUserError:
-              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
-              return
-          await event.delete()
-          await bot.forward_messages(event.chat_id, respond.message)
-
-@register(outgoing=True, pattern="^.hunt(?: |$)(.*)")
+@borg.on(admin_cmd(pattern=r"hunt"))
 async def WooMai(netase):
     if netase.fwd_from:
         return
@@ -72,33 +37,6 @@ async def WooMai(netase):
     await netase.delete()
 
 
-@register(outgoing=True, pattern="^.dzd(?: |$)(.*)")
-async def DeezLoader(Deezlod):
-    if Deezlod.fwd_from:
-        return
-    d_link = Deezlod.pattern_match.group(1)
-    if ".com" not in d_link:
-        await Deezlod.edit("` I need a link to download something pro.`**(._.)**")
-    else:
-        await Deezlod.edit("**Initiating Download!**")
-    chat = "@DeezLoadBot"
-    async with bot.conversation(chat) as conv:
-          try:
-              msg_start = await conv.send_message("/start")
-              response = await conv.get_response()
-              r = await conv.get_response()
-              msg = await conv.send_message(d_link)
-              details = await conv.get_response()
-              song = await conv.get_response()
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await Deezlod.edit("**Error:** `unblock` @DeezLoadBot `and retry!`")
-              return
-          await bot.send_file(Deezlod.chat_id, song, caption=details.text)
-          await Deezlod.client.delete_messages(conv.chat_id,
-                                             [msg_start.id, response.id, r.id, msg.id, details.id, song.id])
-          await Deezlod.delete()          
-    
+
 CMD_HELP.update({
-    "hexa":"this plugin is made by @legendx22 this plugin is not working properly but this is op plugin for poke lovers use .hunt"})
+    "hexa":"this plugin is made by @legendx22 this plugin is now working properly and this  is op plugin for poke lovers use .hunt"})
