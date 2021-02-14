11830cb6182e9434f9510c3512a5e782c77b5da7
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-02-14 12:04:30 +0530

    Mor better

--- userbot/plugins/repo.py
+++ userbot/plugins/repo.py
@@ -1,4 +1,4 @@
-# MADE BY LEGENDX22
+I'm# MADE BY LEGENDX22
 # IDEA BY PROBOY22
 # CREDITS TEAMLEGEND
 # PLEASE KEEP CREDITS 🥺
@@ -13,7 +13,7 @@ from telethon import events, errors, custom, functions
 @tgbot.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
  LEGEND = event.builder
- X= [[custom.Button.inline("Click ME",data="obhai")]]
+ X= [[custom.Button.inline("🔥 CLICK ME 🔥",data="obhai")]]
  query = event.text
  result = LEGEND.article("LEGEND",text="REPO AND SUPPORT",buttons=X,link_preview=False)
  await event.answer([result])
@@ -21,4 +21,4 @@ async def inline_id_handler(event: events.InlineQuery.Event):
 async def callback_query_handler(event):
 
 # inline by LEGENDX22 and PROBOY22 🔥
-  await event.edit(text=f"{BOT} REPO AND GROUP LINK",buttons=[[Button.url(f"{BOT} REPO", url="https://github.com/LEGENDXOP/LEGEND-BOT"), Button.url(f"{BOT} SUPPORT", url="https://t.me/LEGEND_USERBOT_SUPPORT")]])
+  await event.edit(text=f"{BOT} REPO AND GROUP LINK",buttons=[[Button.url(f"🔥{BOT} REPO🔥", url="https://github.com/LEGENDXOP/LEGEND-BOT"), Button.url(f"⚡{BOT} SUPPORT⚡", url="https://t.me/LEGEND_USERBOT_SUPPORT")]])
