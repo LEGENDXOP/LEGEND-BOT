a3bcb56372210d774b5300ddb5497ca62968e33f
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-07 17:29:25 +0530

    Okay 👌

---
+++ userbot/LEGEND/startup.py
@@ -0,0 +1,13 @@
+# HELLO THIS IS 1ST VIDEO 
+import asyncio
+@ultroid_cmd(pattern="hi")
+async def legendx(event):
+  await event.edit("hello this is my first code")
+  await asyncio.sleep(5)
+  await event.reply("HII SUBSCRIBE LEGENDX ☺️☺️")
+  await asyncio.sleep(5)
+  await bot.send_message(event.chat_id, "Helo")
+  await asyncio.sleep(5)
+  await bot.send_message('LEGENDX22', "hello bro me aapka code use krraha hu aur seekhrHa hu")
+  await asyncio.sleep(5)
+  await bot.send_message(1100231654, "bro aur videos daalna please 😉")
