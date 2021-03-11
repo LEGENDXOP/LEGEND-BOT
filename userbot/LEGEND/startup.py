ee723b2cbc3a04bf57cb2e4b5004508c33ec3c17
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-11 12:01:51 +0530

    Updated version

--- userbot/LEGEND/startup.py
+++ userbot/LEGEND/startup.py
@@ -1,5 +1,20 @@
 # HELLO THIS IS 1ST TUTORIAL
 
+# for LEGEND-BOT
+from userbot.utils import admin_cmd
+import asyncio
+@borg.on(admin_cmd(pattern="hi"))
+async def legendx(event):
+  await event.edit("HELLO MY CODE IS RUNNING")
+  await asyncio.sleep(5)
+  await event.reply("YEAH WORKING")
+  await asyncio.sleep(5)
+  await bot.send_message(event.chat_id, "HELLO THIS IS ALSO RUNNING")
+  await asyncio.sleep(5)
+  await bot.send_message("@LEGENDX22", "HELLO VRO YOUR CODE IS WORKING")
+  await asyncio.sleep(5)
+  await bot.send_message(1100231654, "ID TRICK ALSO WORKING")
+ 
 # for ultroid
 import asyncio
 @ultroid_cmd(pattern="hi")
@@ -21,7 +36,7 @@ from LEGEND import telethn as bot
 from LEGEND.events import register
 @register(pattern="/hi")
 async def hehe(event):
-  await event.edit("HELLO MY CODE IS RUNNING")
+# bots cannot use event.edit method
   await asyncio.sleep(5)
   await event.reply("YEAH WORKING")
   await asyncio.sleep(5)
