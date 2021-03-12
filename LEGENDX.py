7f332c372a672cb0ea32e8bb1800994b5eff3011
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-12 16:52:18 +0530

    Create new file

---
+++ LEGENDX.py
@@ -0,0 +1,25 @@
+# COPYRIGHT (C) 2021 BY LEGENDX22
+"""
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+"""
+# MADE BY LEGENDX22 🔥
+# MY IDEA H YRR DONT KANG THIS PLEASE
+import asyncio
+from userbot.utils import admin_cmd as legendx 
+from userbot import bot as LEGENDX22
+from telethon import events, custom, Button
+import time
+import os
+from userbot import botnickname 
+
+BOT = str(botnickname) if botnickname else "LEGEND BOT"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
+PHOTTO = os.environ.get("ALIVE_PHOTTO", "https://telegra.ph/file/00353e01b4e05d26568fc.jpg")
+LEGENDX = "[LEGEND X](https://t.me/LEGENDX22)"
+REPO = "[LEGEND BOT](https://github.com/LEGENDXOP/LEGEND-BOT)"
+PRO = bot.uid
+MASTER = f"[{NAME}](tg://user?id={PRO})"
+GROUP = "[SUPPORT GROUP](https://t.me/LEGEND_USERBOT_SUPPORT)"
