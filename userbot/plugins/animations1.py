d7f1ff07a5619e49e35f227df607f0e8a8f280b9
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-13 12:20:50 +0530

    Update animations1.py

--- userbot/plugins/animations1.py
+++ userbot/plugins/animations1.py
@@ -4,8 +4,8 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot import ALIVE_NAME, CMD_HELP
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+from LEGENDX import NAME
+DEFAULTUSER = NAME
 
 
 @bot.on(admin_cmd(pattern="stupid$"))
@@ -109,7 +109,7 @@ async def _(event):
         usname = replied_user.user.username
         idd = reply_message.sender_id
         if idd==1100231654:
-            await event.edit("This is My Master\n**How dare you trying to tell me to kill master nigger!**\n\n__Your account is on hold! Pay 99$ to my master__ [HellBoy](https://t.me/legendx22) __to release your account__😏")
+            await event.edit("This is My Master\n**How dare you trying to tell me to kill master nigger!**\n\n__Your account is on hold! Pay 99$ to my master__ [LEGENDX](https://t.me/legendx22) __to release your account__😏")
         else:
             await event.edit("`K i l l i n g . . . . . . .`")
             animation_chars = [
