1f3da43766fcb065433a679b9930078f599af72f
PROBOYX <68194873+PROBOYX@users.noreply.github.com>
2021-03-15 14:27:55 +0530

    Fixed
    Done

--- LEGENDX.py
+++ LEGENDX.py
@@ -8,13 +8,13 @@
 # MADE BY LEGENDX22 🔥
 # MY IDEA H YRR DONT KANG THIS PLEASE
 import asyncio
+import os
 from userbot.utils import admin_cmd as legendx
 from userbot import bot as LEGENDX22
 api_id = os.environ.get("APP_ID", None)
 api_hash = os.environ.get("API_HASH", None)
 from telethon import events, custom, Button, TelegramClient
 import time
-import os
 from userbot import botnickname, ALIVE_NAME, bot
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
 xbot = TelegramClient("legend", api_id, api_hash).start(bot_token=token)
