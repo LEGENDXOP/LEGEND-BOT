6082302a5e7bbadf255149af6f6ce55f66cfab1f
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-15 13:33:57 +0530

    Update LEGENDX.py

--- LEGENDX.py
+++ LEGENDX.py
@@ -9,15 +9,14 @@
 # MY IDEA H YRR DONT KANG THIS PLEASE
 import asyncio
 from userbot.utils import admin_cmd as legendx 
-from userbot import APP_ID, API_HASH
+api_id = os.environ.get("APP_ID", None)
+api_hash = os.environ.get("API_HASH", None)
 from telethon import events, custom, Button, TelegramClient
 import time
 import os
 from userbot import botnickname, ALIVE_NAME, bot
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
-xbot = TelegramClient("legend", APP_ID, API_HASH).start(bot_token=token)
-xbot.start()
-x.run_until_disconnected()
+xbot = TelegramClient("legend", api_id, api_hash).start(bot_token=token)
 BOT = str(botnickname) if botnickname else "LEGEND BOT"
 NAME = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
 PHOTO = os.environ.get("ALIVE_PHOTTO", "https://telegra.ph/file/00353e01b4e05d26568fc.jpg")
@@ -26,3 +25,6 @@ REPO = "[LEGEND BOT](https://github.com/LEGENDXOP/LEGEND-BOT)"
 PRO = bot.uid
 MASTER = f"[{NAME}](tg://user?id={PRO})"
 GROUP = "[SUPPORT GROUP](https://t.me/LEGEND_USERBOT_SUPPORT)"
+if __name__=="__main__":
+  xbot.start()
+  xbot.run_until_disconnected()
