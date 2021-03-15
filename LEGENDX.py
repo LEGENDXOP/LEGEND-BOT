93415fc102450dcd0b8c4773a0d40dd0b3007c39
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-15 12:55:18 +0530

    Ok vro

--- LEGENDX.py
+++ LEGENDX.py
@@ -9,12 +9,15 @@
 # MY IDEA H YRR DONT KANG THIS PLEASE
 import asyncio
 from userbot.utils import admin_cmd as legendx 
-from userbot import bot as LEGENDX22
-from telethon import events, custom, Button
+from userbot import APP_ID, API_HASH
+from telethon import events, custom, Button, TelegramClient
 import time
 import os
 from userbot import botnickname, ALIVE_NAME, bot
-
+token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
+xbot.start()
+x.run_until_disconnected()
 BOT = str(botnickname) if botnickname else "LEGEND BOT"
 NAME = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
 PHOTO = os.environ.get("ALIVE_PHOTTO", "https://telegra.ph/file/00353e01b4e05d26568fc.jpg")
