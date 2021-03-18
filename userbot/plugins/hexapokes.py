580b39ce450a24a68dbc425c65d88886b63a71ee
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-18 19:52:23 +0530

    Update hexapokes.py

--- userbot/plugins/hexapokes.py
+++ userbot/plugins/hexapokes.py
@@ -9,8 +9,9 @@ naam = str(ALIVE_NAME)
 
 bot = "@hexamonbot"
 
+Pokes = ('**Here Are Your HexaPokes**\n➖➖➖➖➖➖➖➖➖➖➖\n\n')
 
-@borg.on(admin_cmd("mypokes ?(.*)"))
+@borg.on(admin_cmd("mypokes(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -18,36 +19,29 @@ async def _(event):
     if sysarg == "":
         async with borg.conversation(bot) as conv:
             try:
-                await conv.send_message("/start")
-                await conv.get_response()
                 await conv.send_message("/mypokemon")
                 audio = await conv.get_response()
-                await borg.send_message(event.chat_id, audio.text)
+                await borg.send_message(event.chat_id, Pokes + audio.text)
                 await event.delete()
             except YouBlockedUserError:
-                await event.edit("Error: unblock @hexamonbot and retry!")
+                await event.edit("Error: unblock **@hexamonbot** and retry!")
     elif "@" in sysarg:
         async with borg.conversation(bot) as conv:
             try:
-                await conv.send_message("/start")
-                await conv.get_response()
                 await conv.send_message("/mypokemon " + sysarg)
                 audio = await conv.get_response()
-                await borg.send_message(event.chat_id, audio.text)
+                await borg.send_message(event.chat_id, Pokes + audio.text)
                 await event.delete()
             except YouBlockedUserError:
-                await event.edit("**Error:** unblock @hexamonbot and try again!")
+                await event.edit("Error: unblock **@hexamonbot** and try again!")
     elif "" in sysarg:
         async with borg.conversation(bot) as conv:
             try:
-                await conv.send_message("/start")
-                await conv.get_response()
                 await conv.send_message("/mypokemon " + sysarg)
                 audio = await conv.get_response()
-                await borg.send_message(event.chat_id, audio.text)
+                await borg.send_message(event.chat_id, Pokes + audio.text)
                 await event.delete()
             except YouBlockedUserError:
-                await event.edit("Error: unblock @hexamonbot `and try again!")
+                await event.edit("Error: unblock **@hexamonbot** and try again!")
 CMD_HELP.update({
-    "hexapokes":"type .mypokes to get your all hexa pokes"})
-
+    "hexapokes":"type `.mypokes` to get your all hexa pokes"})
