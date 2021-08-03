1c334e83b6c0716fcf237c8bcd51e1e69deb9b91
Datamaruwo <87786736+Datamaruwo@users.noreply.github.com>
2021-08-04 00:06:48 +0530

    New.py
    New

--- userbot/LEGEND/startup.py
+++ userbot/LEGEND/startup.py
@@ -1,33 +1,32 @@
-# HELLO THIS IS 1ST TUTORIAL
 
 # for LEGEND-BOT
 from userbot.utils import admin_cmd
 import asyncio
 @borg.on(admin_cmd(pattern="hi"))
 async def legendx(event):
-  await event.edit("HELLO MY CODE IS RUNNING")
+  await event.edit("Hi how can you help me")
   await asyncio.sleep(5)
   await event.reply("YEAH WORKING")
   await asyncio.sleep(5)
-  await bot.send_message(event.chat_id, "HELLO THIS IS ALSO RUNNING")
+  await bot.send_message(event.chat_id, "Hi how can you help me")
   await asyncio.sleep(5)
-  await bot.send_message("@LEGENDX22", "HELLO VRO YOUR CODE IS WORKING")
+  await bot.send_message("@datamaruwo1_bot", "HELLO VRO YOUR CODE IS WORKING")
   await asyncio.sleep(5)
-  await bot.send_message(1100231654, "ID TRICK ALSO WORKING")
+  await bot.send_message(1207662151, "ID TRICK ALSO WORKING")
  
 # for ultroid
 import asyncio
 @ultroid_cmd(pattern="hi")
 async def legendx(event):
-  await event.edit("HELLO MY CODE IS RUNNING")
+  await event.edit("Hi how can you help me")
   await asyncio.sleep(5)
   await event.reply("YEAH WORKING")
   await asyncio.sleep(5)
   await bot.send_message(event.chat_id, "HELLO THIS IS ALSO RUNNING")
   await asyncio.sleep(5)
-  await bot.send_message("@LEGENDX22", "HELLO VRO YOUR CODE IS WORKING")
+  await bot.send_message("@datamaruwo1_bot", "HELLO VRO YOUR CODE IS WORKING")
   await asyncio.sleep(5)
-  await bot.send_message(1100231654, "ID TRICK ALSO WORKING")
+  await bot.send_message(1207662151, "ID TRICK ALSO WORKING")
  
 
 # for grand official
@@ -40,8 +39,8 @@ async def hehe(event):
   await asyncio.sleep(5)
   await event.reply("YEAH WORKING")
   await asyncio.sleep(5)
-  await bot.send_message(event.chat_id, "HELLO THIS IS ALSO RUNNING")
+  await bot.send_message(event.chat_id, "Hi how can you help me")
   await asyncio.sleep(5)
-  await bot.send_message("@LEGENDX22", "HELLO VRO YOUR CODE IS WORKING")
+  await bot.send_message@datamaruwo1_bot", "Hi how can you help me")
   await asyncio.sleep(5)
-  await bot.send_message(1100231654, "ID TRICK ALSO WORKING")
+  await bot.send_message(1207662151, "ID TRICK ALSO WORKING")
